e3eeedc53fd2e38585f738b3a2c766cdf5d45bda
Michelle Zhou <zz635793526@gmail.com>
2024-02-11 23:31:06 +0000

    change title and add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -18,7 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(count + 1)}>
           count is {count}
         </button>
         <p>
@@ -27,8 +28,20 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </p> */
+    /* </div> */}
+    <div>
+      <span id="logo-img">logo</span>
+      <span className="navigation-bar">
+        <span>Recipe book</span>
+        <span>Planning</span>
+        <span>Shopping list</span>
+        <li>
+          <Link to="/new-route">New Route</Link>
+        </li>
+      </span>
+    </div>
+  </>
   )
 }
 
