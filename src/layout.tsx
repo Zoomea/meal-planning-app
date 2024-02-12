c7626bd1e7c6444835f3a4d8f9aba1417b5dcea1
Michelle Zhou <zz635793526@gmail.com>
2024-02-12 17:29:25 +0000

    add navigation bar

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,25 +1,41 @@
 import { Outlet, Link } from "react-router-dom";
+import siteLogo from './assets/siteLogo.png';
+import recipeLogo from './assets/recipe.png';
+import calendarLogo from './assets/calendar.png';
+import shoppingLogo from './assets/shopping.png';
+import "./App.css";
 
 const Layout = () => {
     return (
-      <>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Recipe book</Link>
-            </li>
-            <li>
-              <Link to="/calendar">Planning</Link>
-            </li>
-            <li>
-              <Link to="/shoppingList">Shopping list</Link>
-            </li>
-          </ul>
-        </nav>
-  
-        <Outlet />
-      </>
+        <>
+            <div className="navigation-bar">
+                <img id="site-logo" src={siteLogo} alt="site logo" />
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">
+                                <img src={recipeLogo} alt="recipe logo" />
+                                <Link to="/">Recipe book</Link>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/calendar">
+                                <img src={calendarLogo} alt="calendar logo" />
+                                <Link to="/calendar">Planning</Link>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/shoppingList">
+                                <img src={shoppingLogo} alt="shopping logo" />
+                                <Link to="/shoppingList">Shopping list</Link>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <Outlet />
+        </>
     )
-  };
-  
-  export default Layout;
+};
+
+export default Layout;
