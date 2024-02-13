76f35a653e87166d94c011a4554ba9b7cd767978
Michelle Zhou <zz635793526@gmail.com>
2024-02-13 16:30:13 +0000

    add green color configuration in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./src/**/*.{html,js,jsx,tsx,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          '100': "#EAEFC5",
+          '200': '#BDE09E',
+          '300': '#90BB7C',
+          '400': '#5F9256',
+          '500': '#497042',
+          '600': '#34502F',
+          '700': '#1F301C',
+        },
+      },
+    },
   },
   plugins: [],
 }
