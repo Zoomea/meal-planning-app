ce5b254aa55851cbfdfcbaf035f1c85579410420
Michelle Zhou <zz635793526@gmail.com>
2024-02-13 15:45:53 +0000

    equal space on left/right in navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './layout';
 import Calendar from './calendar-view';
