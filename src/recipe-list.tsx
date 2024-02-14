a24d9263a11c13b9d70bc27453b515913810c250
Michelle Zhou <zz635793526@gmail.com>
2024-02-14 19:43:11 +0000

    add hovering effect on buttons and recipe component

--- src/recipe-list.tsx
+++ src/recipe-list.tsx
@@ -4,12 +4,12 @@ const Recipes = () => {
     return <div className="w-3/4 mx-auto">
         <div className="recipe-btns flex justify-between">
             <div className="recipe-btns-left flex">
-                <div className="bg-green-500 text-white py-2 px-4 rounded-full hover:bg-green-600 mr-4">Main</div>
-                <div className="bg-green-500 text-white py-2 px-4 rounded-full hover:bg-green-600">Dessert</div>
+                <div className="bg-green-500 text-white py-2 px-4 rounded-full hover:bg-green-600 mr-4 hover:shadow-lg transition">Main</div>
+                <div className="bg-green-500 text-white py-2 px-4 rounded-full hover:bg-green-600 hover:shadow-lg transition">Dessert</div>
             </div>
             <div className="recipe-btns-right flex">
-                <div className="bg-green-500 text-white py-2 px-4 rounded-full hover:bg-green-600 mr-4">Search</div>
-                <div className="bg-green-500 text-white py-2 px-4 rounded-full hover:bg-green-600">+ Add new recipe</div>
+                <div className="bg-green-500 text-white py-2 px-4 rounded-full hover:bg-green-600 mr-4 hover:shadow-lg transition">Search</div>
+                <div className="bg-green-500 text-white py-2 px-4 rounded-full hover:bg-green-600 hover:shadow-lg transition">+ Add new recipe</div>
             </div>
         </div>
         {/* get the data from backend and use map to show each one */}
--- src/recipe.tsx
+++ src/recipe.tsx
@@ -2,7 +2,7 @@ import siteLogo from './assets/siteLogo.png';
 import downArrow from './assets/down-arrow.svg';
 
 const Recipe = () => {
-    return <div className="w-full h-48 bg-green-300 rounded-xl hover:shadow-md transition">
+    return <div className="w-full h-48 bg-green-300 rounded-xl hover:shadow-lg transition">
         <img className="w-full h-4/5 rounded-t-xl" src={siteLogo} alt="recipe pic" />
         <div className="mx-4 h-1/5 flex justify-between items-center text-green-700">
             <div>Name</div>
