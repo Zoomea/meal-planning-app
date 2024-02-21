adbd914ad85e81cf9293ae211c38f6bb4da5e5d8
Michelle Zhou <zz635793526@gmail.com>
2024-02-21 21:12:51 +0000

    add calendar from and to

--- src/shopping-list.tsx
+++ src/shopping-list.tsx
@@ -1,5 +1,7 @@
 const ShoppingList = () => {
     return <div>
+        <div>calendar from</div>
+        <div>calendar to</div>
         <div className="border border-green-500 rounded mx-8">shopping list</div>
     </div>;
   };
