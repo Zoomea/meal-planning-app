954e952a9575e2a06cfb582589e48aedf4de096a
Michelle Zhou <zz635793526@gmail.com>
2024-04-04 20:02:16 +0100

    add generate shopping list button

--- src/shopping-list.tsx
+++ src/shopping-list.tsx
@@ -5,11 +5,12 @@ const ShoppingList = () => {
     return <div>
         <div className="mx-8">
             <DateRangePicker />
+            <button className="border rounded p-2 bg-green-300 hover:bg-green-400">Generate shopping list</button>
         </div>
         <div className="border border-green-500 rounded mx-8">shopping list</div>
         <div className="mx-8">
             <NewRecipeModal />
-        </div>
+        </div> 
     </div>;
   };
   
